a47e19abb5d8d0b88675a2514796910b0d91e487
Ptomerty <Ptomerty@users.noreply.github.com>
2019-03-10 11:00:32 -0400

    drop in public files

---
+++ answer_phone.py
@@ -0,0 +1,53 @@
+import os
+from flask import Flask, request
+from twilio.twiml.voice_response import VoiceResponse, Dial, Say
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
+
+app = Flask(__name__)
+# Your Account Sid and Auth Token from twilio.com/user/account
+account_sid = os.getenv("account_sid")
+auth_token = os.getenv("auth_token")
+client = Client(account_sid, auth_token)
+
+forbidden_states = ["CA", "CT", "FL", "IL", "MD", "MA", "MT", "NH", "PA", "WA"]
+
+@app.route("/sms", methods=['GET','POST'])
+def reply():
+    if request.values.get("FromState", None) in forbidden_states:
+        resp = MessagingResponse()
+        resp.message("We're sorry, but we cannot bake any bread in your area!")
+        return str(resp)
+    else:
+        number = request.values.get("From", None)
+        call = client.calls.create(
+                        url='http://hackru.plup.club:5000/call_xml',
+                        to=number,
+                        from_='+16092566938',
+                        record=True,
+                        recording_channels='dual',
+                    )
+        return ""
+
+@app.route("/call_xml", methods=['GET','POST'])
+def call_xml():
+    resp = VoiceResponse()
+    resp.say("Thank you for calling Ivylands Bakery. Please hold until" +
+        "  the next representative is available.", voice='alice')
+    resp.play('https://my.mixtape.moe/qhqkqa.mp3') # 4minhold
+    return str(resp)
+
+@app.route("/voice", methods=['GET', 'POST'])
+def voice():
+    """Respond to incoming phone calls and mention the caller's city"""
+    if request.values.get("FromState", None) in forbidden_states:
+        resp = VoiceResponse()
+        resp.say("We're sorry, but we cannot bake any bread in your area.")
+        resp.hangup()
+        return str(resp)
+    else:
+        resp = call_xml()
+        return str(resp)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export account_sid=YOUR_SID_HERE
+export auth_token=YOUR_TOKEN_HERE
+python answer_phone.py
